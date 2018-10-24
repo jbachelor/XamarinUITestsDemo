b11929480a9a4bd8e0b762dc109fd4a69f892173
Luxor Mob <mobluxor1user@hunterindustries.com>
2018-10-24 09:41:14 -0700

    Added in retries for app launch failures. Now getting iOS tests passing more frequently, AFTER deleting DeviceAgent app from our test device. Might be good to uninstall DeviceAgent any time there is a deviceAgent failure... Not sure if that's possible.

--- RegularUITestProj/AppInitializer.cs
+++ RegularUITestProj/AppInitializer.cs
@@ -10,33 +10,56 @@ namespace RegularUITestProj
     {
         public static IApp StartApp(Platform platform)
         {
-            // TODO: If the iOS or Android app being tested is included in the solution 
-            // then open the Unit Tests window, right click Test Apps, select Add App Project
-            // and select the app projects that should be tested.
-            //
-            // The iOS project should have the Xamarin.TestCloud.Agent NuGet package
-            // installed. To start the Test Cloud Agent the following code should be
-            // added to the FinishedLaunching method of the AppDelegate:
-            //
-            //    #if ENABLE_TEST_CLOUD
-            //    Xamarin.Calabash.Start();
-            //    #endif
+            IApp app = null;
+            int maxAttempts = 5;
+            bool appDidLaunch = false;
+
+            for (int i = 0; i < maxAttempts; i++)
+            {
+                Console.WriteLine($"**** {nameof(AppInitializer)}.{nameof(StartApp)}:  Attempt {i + 1}");
+                try
+                {
+                    app = TryToLaunchAppOnPlatform(platform);
+                    appDidLaunch = true;
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"**** {nameof(AppInitializer)}.{nameof(StartApp)}:  FAILED TO LAUNCH APP WITH EXCEPTION:\n{ex}\n\n\n\n");
+                }
+            }
+
+            if (appDidLaunch == false)
+            {
+                Console.WriteLine($"\n\n\n**** App failed to launch even after 5 attempts. Giving up.\n\n\n");
+            }
+
+            return app;
+        }
+
+        private static IApp TryToLaunchAppOnPlatform(Platform platform)
+        {
+            IApp app;
             if (platform == Platform.Android)
             {
-                return ConfigureApp
+                app = ConfigureApp
                     .Android
                     // TODO: Update this path to point to your Android app and uncomment the
                     // code if the app is not included in the solution.
                     //.ApkFile ("../../../Droid/bin/Debug/xamarinforms.apk")
                     .StartApp();
             }
-
-            return ConfigureApp
+            else
+            {
+                app = ConfigureApp
                 .iOS
                 // TODO: Update this path to point to your iOS app and uncomment the
                 // code if the app is not included in the solution.
                 //.AppBundle ("../../../iOS/bin/iPhoneSimulator/Debug/XamarinForms.iOS.app")
                 .StartApp();
+            }
+
+            return app;
         }
     }
 }
